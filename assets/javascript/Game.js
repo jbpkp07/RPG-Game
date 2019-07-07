84ae2c3f81b17d9ade8d3ef8f7ece73ab35e7e6e
jbpkp07 <jeremy.w.barnes@gmail.com>
2019-07-07 03:26:30 -0700

    updates in all areas: html, css, images...

--- assets/javascript/Game.js
+++ assets/javascript/Game.js
@@ -3,34 +3,54 @@
 //-----------------------------------------------------------------------------
 //Note:  For this assignment Jason has challenged me to do this WITHOUT using 
 //       an object-oriented approach.
-//       While I can't "unsee" the benefits of architecting soltutions using an OO approach,
+//       While I can't "unsee" the benefits of architecting solutions using an OO approach,
 //       the following is an attempt at doing this through a procedural approach.
 //       #StayChallenged   :-)
 //-----------------------------------------------------------------------------
 
-
-
 //Global Variables-------------------------------------------------------------
-let waitToInterval;
-const startBtn = $("#startLogoWrapper");
+let startBtn;
+
+let yoda;
+let leia;
+let vader;
+let luke;
+let players = [];
+
+let intervalID;
 
 
-//Objects / Listeners----------------------------------------------------------
-startBtn.click(startGame);
+//Objects ---------------------------------------------------------------------
+
 
 
 //Functions--------------------------------------------------------------------
+function initialize() {
+
+    yoda = $("#yodaWrapper");
+    leia = $("#leiaWrapper");
+    vader = $("#vaderWrapper");
+    luke = $("#lukeWrapper");
+
+    players = [yoda, leia, vader, luke];
+
+    startBtn = $("#startLogoWrapper");
+    startBtn.click(startGame);
+
+    waitToStartGame();
+}
+
 function waitToStartGame() {
 
     let blurred;
 
-    setTimeout(() => {
+    setTimeout( () => {
 
         startBtn.addClass("startLogoWrapperBlur");
         blurred = true;
     }, 10);
 
-    waitToInterval = setInterval(() => {
+    intervalID = setInterval( () => {
 
         if (!blurred) {
             startBtn.addClass("startLogoWrapperBlur");
@@ -46,21 +66,93 @@ function waitToStartGame() {
 
 function startGame() {
 
-    clearInterval(waitToInterval);
+    clearInterval(intervalID);
 
     startBtn.removeClass("startLogoWrapperBlur");
 
-    setTimeout(() => {
+    setTimeout( () => {
 
-        startBtn.addClass("startLogoWrapperFade"); 
-        
-        setTimeout(() => {
-            startBtn.attr("class", "startLogoWrapperHide");
+        startBtn.addClass("startLogoWrapperFade");
+
+        setTimeout( () => {
+
+            startBtn.attr("class", "hidden");
+
+            selectPlayer();
         }, 5000);
 
     }, 1500);
 }
 
+function selectPlayer() {
+
+    let durationMS = 2000;
+    let delayMs = 0;
+
+    for (let player of players) {
+
+        player.removeClass("hidden");
+
+        setTimeout( () => {
+
+            if (player === players[players.length - 1]) {
+
+                player.animate({ top: '50%' }, durationMS).promise().done( () => {
+                        assignSelectListeners();
+                    });
+            }
+            else {
+
+                player.animate({ top: '50%' }, durationMS);
+            }
+
+        }, delayMs);
+
+        delayMs += 333;   
+    }
+}
+
+function assignSelectListeners() {
+
+    for (let player of players) {
+
+        player.click( () => {
+            alert(player.attr("value"));
+        });
+
+        player.hover(
+            () => {
+                
+                player.removeClass("playerBorder");
+                
+                player.addClass("playerSelect");
+    
+                for (let otherPlayer of players) {
+    
+                    if (otherPlayer !== player) {
+    
+                        otherPlayer.addClass("playerBlur");
+                    }
+                }
+            },
+            () => {
+                
+                player.removeClass("playerSelect");
+                
+                player.addClass("playerBorder");
+    
+                for (let otherPlayer of players) {
+    
+                    if (otherPlayer !== player) {
+    
+                        otherPlayer.removeClass("playerBlur");
+                    }
+                }
+            }
+        );
+    }
+}
+
 //Calls------------------------------------------------------------------------
-waitToStartGame();
+$(document).ready(initialize);
 
